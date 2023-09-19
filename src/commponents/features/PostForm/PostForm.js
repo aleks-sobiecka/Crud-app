f1acdd9a78866b2d1d07f2f3b3c021963a43ec04
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-09-19 17:29:53 +0200

    Add form validate

--- src/commponents/features/PostForm/PostForm.js
+++ src/commponents/features/PostForm/PostForm.js
@@ -6,42 +6,58 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useForm } from "react-hook-form";
 
 const PostForm = ({ action, actionText, ...props }) => {
+
+    const { register, handleSubmit: validate, formState: { errors } } = useForm();
 
     const [title, setTitle] = useState(props.title || '');
     const [author, setAuthor] = useState(props.author || '');
     const [publishedDate, setPublishedDate] = useState(props.publishedDate || '');
     const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
     const [content, setContent] = useState(props.content || '');
+    const [contentError, setContentError] = useState(false);
+    const [dateError, setDateError] = useState(false);
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        action({ title, author, publishedDate, shortDescription, content });
+    const handleSubmit = () => {
+        setContentError(!content)
+        setDateError(!publishedDate)
+        if(content && publishedDate) {
+            action({ title, author, publishedDate, shortDescription, content })
+        }
       };
     
     
     return (
-        <Form onSubmit={handleSubmit} className="">
+        <Form onSubmit={validate(handleSubmit)} className="">
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{ width: '300px' }}>
                 <Form.Label>Title</Form.Label>
-                <Form.Control type="text" placeholder="Enter title" value={title} onChange={e => setTitle(e.target.value)} />
+                <Form.Control {...register("title", { required: true, minLength: 3 })} 
+                type="text" placeholder="Enter title" value={title} onChange={e => setTitle(e.target.value)} />
+                {errors.title && <small className="d-block form-text text-danger mt-2">Title is too short (min is 3)</small>}
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{ width: '300px' }}>
                 <Form.Label>Author</Form.Label>
-                <Form.Control type="text" placeholder="Enter author" value={author} onChange={e => setAuthor(e.target.value)} />
+                <Form.Control {...register("title", { required: true, minLength: 3 })} 
+                type="text" placeholder="Enter author" value={author} onChange={e => setAuthor(e.target.value)} />
+                {errors.title && <small className="d-block form-text text-danger mt-2">Author is too short (min is 3)</small>}
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{ width: '300px' }}>
                 <Form.Label>Published</Form.Label><br />
                 <ReactDatePicker placeholder="Enter date" selected={publishedDate} onChange={(date) => setPublishedDate(date)} />
+                {contentError && <small className="d-block form-text text-danger mt-2">This field is required</small>}
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" style={{ minWidth: '300px' }}>
                 <Form.Label>Short description</Form.Label>
-                <Form.Control as="textarea" rows={3} placeholder="Leave a comment here" value={shortDescription} onChange={e => setShortDescription(e.target.value)} />
+                <Form.Control {...register("title", { required: true, minLength: 20 })} 
+                as="textarea" rows={3} placeholder="Leave a comment here" value={shortDescription} onChange={e => setShortDescription(e.target.value)} />
+                {errors.title && <small className="d-block form-text text-danger mt-2">Description is too short (min is 20)</small>}
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" style={{ minWidth: '300px' }}>
                 <Form.Label>Main content</Form.Label>
                 <ReactQuill theme="snow" value={content} onChange={setContent} />
+                {contentError && <small className="d-block form-text text-danger mt-2">This field is required</small>}
             </Form.Group>
             <Button type="submit">{actionText}</Button>
         </Form>
